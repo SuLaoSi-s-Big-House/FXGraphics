8c07d82bc79c11c228c1aa0fcf7f6791ce256d09
苏靖翔 <sujingxiang@zwcad.com>
2025-11-11 15:14:21 +0800

    add shader deployment

--- FXMain/src/main.cpp
+++ FXMain/src/main.cpp
@@ -1,5 +1,44 @@
+#include <fstream>
+#include "graphics_window.h"
+#include "graphics_printer.h"
 
 int main()
 {
+    FX::GraphicsNormalPrinter printer1(FX::PrintType::kPoints);
 
+    FX::GraphicsWindow window1(800, 600);
+    FX::GraphicsWindow window2(400, 300);
+    window1.use();
+
+    FX::GraphicsNormalPrinter printer2(FX::PrintType::kTriangles);
+
+    std::ifstream ifs;
+    ifs.open("./shader/normal_world.vert");
+    printer1.addShader(FX::GPUItemType::kVtxShader, ifs);
+    ifs.close();
+    ifs.open("./shader/normal_world.vert");
+    printer2.addShader(FX::GPUItemType::kVtxShader, ifs);
+    ifs.close();
+    ifs.open("./shader/normal_world.frag");
+    printer1.addShader(FX::GPUItemType::kFrgShader, ifs);
+    ifs.close();
+    ifs.open("./shader/normal_world.frag");
+    printer2.addShader(FX::GPUItemType::kFrgShader, ifs);
+    ifs.close();
+
+    window1.use();
+    window1.frame();
+
+    printer1.use();
+
+    window1.frame();
+
+    window2.use();
+
+    printer1.use();
+    printer2.use();
+
+    window2.frame();
+
+    window1.use();
 }
---
+++ resources/shader/normal_input.h
@@ -0,0 +1,4 @@
+﻿layout(location = 0) in vec3 aPos;
+layout(location = 1) in vec3 aNormal;
+layout(location = 2) in vec3 aUv;
+layout(location = 3) in ivec2 aRank;
